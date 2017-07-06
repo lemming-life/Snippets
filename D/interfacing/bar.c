f8f24f7218ad67255d4af7bde46a8f7103ec14fd
lemming-life <email@lemming.life>
2017-07-06 11:49:27 -0600

    D interfacing with C++ and C example.

---
+++ D/interfacing/bar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+char bar(char c) {
+    printf("Input character is %c. Output is ", c);
+    return 'b';
+}
---
+++ D/interfacing/main.d
@@ -0,0 +1,24 @@
+// Author: http://lemming.life
+// Description: Small example on how D interfaces with C++ and C.
+// - There is also a small description on how to compile the files.
+
+/* At terminal do:
+    g++ -c foo.cpp
+    gcc -c bar.c
+    dmd -c main.d
+    dmd main.o foo.o bar.o
+*/
+
+extern(C++) int foo(int a, int b);
+extern(C) char bar(char c);
+
+void main() {
+    import std.stdio : writeln;
+    foo(3, 5).writeln;
+    bar('a').writeln;
+}
+
+/* Expected output:
+8
+Input character is a. Output is b
+*/
