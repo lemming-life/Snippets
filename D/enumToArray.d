511e614dccebc13e610333dba838a7432125a7ab
lemming-life <email@lemming.life>
2017-07-20 12:39:07 -0600

    Added test to enumToArray.d

--- D/enumToArray.d
+++ D/enumToArray.d
@@ -2,6 +2,8 @@
 // Language: D
 // Purpose: Get members of Enum E and convert them to items of an array of type T
 // Date: July 17, 2017
+// Test: rdmd -unittest -main enumToArray.d
+// - Expected: Passed
 
 enum Colors {Red, Green, Blue}
 enum Numbers : int { One = 1, Two, Three }
@@ -18,31 +20,15 @@ auto enumToArray(E, T)() {
 }
 
 
-void main(string[] args) {
-    string[] colorsAsString = enumToArray!(Colors, string);
-    colorsAsString.printArray;
-
-    dstring[] numbersAsDString = enumToArray!(Numbers, dstring);
-    numbersAsDString.printArray;
-
-    int[] numbersAsInt = enumToArray!(Numbers, int);
-    numbersAsInt.printArray;
-}
-
 auto printArray(T)(T[] array) {
     import std.stdio : writeln;
     foreach (item ; array) { item.writeln; }
 }
 
-// Output:
-/*
-Red
-Green
-Blue
-One
-Two
-Three
-1
-2
-3
-*/
+unittest {
+    assert( enumToArray!(Colors, string) == ["Red", "Green", "Blue"] ); // a string[]
+    assert(enumToArray!(Numbers, dstring) == ["One"d, "Two"d, "Three"d] ); // a dstring[]
+    assert( enumToArray!(Numbers, int) == [1, 2, 3] ); // a int[]
+    import std.stdio : writeln;
+    "Passed".writeln;
+}
