d18a83c696dfdaab189748a83b19e0000280b264
lemming-life <email@lemming.life>
2017-07-20 21:42:39 -0600

    Added clipboard.d OSX and Linux text supported.

---
+++ D/clipboard.d
@@ -0,0 +1,82 @@
+// Author: http://lemming.life
+// Language: D
+// Description: A class with access to clipboard features.
+// Testing:
+// rdmd -unittest -main clipboard.d
+
+module clipboard;
+
+// Info:
+// Linux xclip : http://linux.softpedia.com/get/Text-Editing-Processing/Others/xclip-42705.shtml
+// OSX pbcopy pbpaste : https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/pbpaste.1.html
+// Windows Clipboard : https://msdn.microsoft.com/en-us/library/windows/desktop/ms648709(v=vs.85).aspx
+
+import std.conv : to;
+
+// Import platform specific
+version (Windows) {
+    extern (Windows) {
+        
+    }
+} else {
+    import std.algorithm : joiner, map, reduce;
+    import std.array;
+    import std.process : pipeProcess, ProcessPipes, Redirect, wait;
+}
+
+class Clipboard {
+    static dstring readText() {
+        version ( Windows ) {
+            // todo
+        } else {
+            ProcessPipes pipes;
+            scope(exit) { wait(pipes.pid); }
+
+            version(linux) {
+                pipes = pipeProcess(["xclip", "-o", "-selection", "clipboard"], Redirect.stdout);
+            } else version (OSX) {
+                pipes = pipeProcess(["pbpaste"], Redirect.stdout);
+            }
+
+            auto message = pipes.stdout.byChunk(4096).joiner.array;
+            return message.length == 0 ? ""
+                    : reduce!( (soFar,b) => soFar ~= b ) (message.map!( a=> to!dstring( to!dchar(a) )));
+        }
+    }
+
+    static void writeText(dstring text) {
+        version ( Windows ) {
+            // todo
+        } else {
+            ProcessPipes pipes;
+            scope(exit) { wait(pipes.pid); }
+            version(linux) {
+                pipes = pipeProcess(["xclip", "-i", "-selection", "clipboard"], Redirect.stdin);
+            } else version (OSX) {
+                pipes = pipeProcess(["pbcopy"], Redirect.stdin);
+            }
+
+            with(pipes) {
+                stdin.write(text);
+                stdin.flush();
+                stdin.close();
+            }
+        }
+    }
+
+    static void clear() {
+        writeText(""d);
+    }
+}
+
+
+unittest {
+    import std.stdio : writeln;
+    auto testString = "Test String"d;
+    
+    Clipboard.clear();
+    assert( Clipboard.readText().length == 0 );
+
+    Clipboard.writeText(testString);
+    assert( Clipboard.readText() == testString); //.writeln;
+}
