5bb26cf0ebd0f1d145723c4ce09081ec05c2b4e6
lemming-life <email@lemming.life>
2017-07-30 15:21:32 -0600

    Added a timing example using threads and stop watch.

---
+++ D/timing.d
@@ -0,0 +1,32 @@
+// Author: http://lemming.life
+// Language: D
+// Description: Spawn threads, make each wait for some time, then execute a function.
+
+// Test: rdmd -unittest -main timing.d
+
+import std.datetime : StopWatch;
+import std.concurrency : spawn;
+import std.datetime;
+
+void worker(int seconds, ref StopWatch sw) {
+    sw.start();
+    while(sw.peek().to!("seconds", int)() < seconds) { }
+    sw.stop();
+    sw.reset();
+    someFunction(seconds);
+}
+
+void someFunction(int seconds) {
+    import std.stdio : writeln;
+    import std.conv : to;
+    writeln("Hello, World! Seconds: " ~ to!string(seconds));
+}
+
+unittest {
+    import std.stdio : writeln;
+    "Starting workers".writeln;
+    StopWatch sw1, sw2;
+    spawn(&worker, 4, sw1);  // worker will call someFunction in 4 seconds.
+    spawn(&worker, 2, sw2);  // worker will call someFunction in 2 seconds.
+    "Workers are still running ...".writeln;
+}
