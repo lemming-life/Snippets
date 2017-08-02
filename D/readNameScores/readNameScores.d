3442547926183cc6c70528391589caeda3a6c4db
lemming-life <email@lemming.life>
2017-08-01 22:11:25 -0600

    Added simple read from file example.

---
+++ D/readNameScores/readNameScores.d
@@ -0,0 +1,30 @@
+// Author: http://lemming.life
+// Language: D
+// Description: Reads from file a list of names and scores and outputs
+//  those names and scores which have a score of 90 or greater.
+// Data looks like: Thomas Edison, 55
+
+// Running it:
+// rdmd readNameScores.d nameScores.txt
+
+import std.stdio : writeln, File;
+import std.conv : to, parse;
+import std.string : indexOf;
+
+void main(string[] args) {
+    auto file = File(args[1]);
+    const int A_SCORE = 90;
+  
+    string line;
+    while( (line = file.readln) !is null) {
+        int index = to!int(line.indexOf(","));
+        string name = line[0 .. index];
+
+        string scoreString = line[index+2 .. line.length];
+        int score = parse!int(scoreString);
+
+        if (score >= A_SCORE) {
+            writeln(name ~ " - " ~ to!string(score));
+        }
+    }
+}
