90795f3e98cfdda6cc70f137a6c0f26f7c5dd1f9
lemming-life <email@lemming.life>
2017-08-12 20:46:44 -0600

    Added a removeAt method to vector.

--- CPP/simpleVector.cpp
+++ CPP/simpleVector.cpp
@@ -27,37 +27,40 @@ class Vector {
             }
         }
     public:
-        Vector() {
-            initData();
-        }
-        ~Vector() {
-            clearData();
-        }
+        Vector() { initData(); }
+        ~Vector() { clearData(); }
+
+        int size() const { return _size; }
+        int capacity() const { return _capacity; }
 
         void clear() {
             clearData();
             initData();
         }
 
-        int capacity() const {
-            return _capacity;
-        }
-
-        int size() const {
-            return _size;
-        }
-
         T at(int index) {
-            if ( index < 0 || index > _size-1 ) {
-                throw index;
-            } else {
+            if ( index < 0 || index > _size-1 ) { throw index; }
             return _data[index];
-                
-            }
+        }
+
+        void removeAt(int index) {
+            if (index < 0 || index > _size-1 ) { throw index; }
+
+            T* newArray = new T[_size-1];
+
+            // Grab leftOf
+            for (int i=0; i<index; ++i) { newArray[i] = _data[i]; }
+
+            // Grab rightOf
+            for (int i=index+1; i<_size; ++i) { newArray[i-1] = _data[i]; }
+
+            delete[] _data;
+            _data = newArray;
+
+            --_size;
         }
 
         void push(T item) {
-
             // Increase size if needed, copy old values to new array
             if (_size > _capacity-1) {
                 _capacity = _capacity * 2;
@@ -77,22 +80,45 @@ class Vector {
         string toString() {
             return "Size: " + to_string(_size) + ", Capacity: " + to_string(_capacity);
         }
+
+        void print(string separator) {
+            for (int i=0; i<size(); ++i) {
+                cout << at(i) << separator;
+            }
+        }
 };
 
 int main() {
+    cout << "Int Vector\n";
     Vector<int> myIntVector;
     for(int i=0; i<6; ++i) { myIntVector.push(i); }
-    for (int i=0; i<myIntVector.size(); ++i) { cout << myIntVector.at(i) << " "; }
+    myIntVector.print(" ");
     cout << '\n';
     cout << myIntVector.toString() << "\n";
     cout << "Clearing vector.\n";
     myIntVector.clear();
     cout << myIntVector.toString() << "\n";
 
+    cout << "Char Vector\n";
     Vector<char> myCharVector;
     for (int i=0; i<5; ++i) { myCharVector.push(65+i); }
-    for (int i=0; i<myCharVector.size(); ++i) { cout << myCharVector.at(i) << " "; }
+    myCharVector.print(" "); // A B C D E
     cout << '\n';
+    myCharVector.removeAt(2);
+    myCharVector.print(" "); // A B D E
+    cout << "\n"; 
+    myCharVector.removeAt(0);
+    myCharVector.print(" "); // B D E
+    cout << "\n";
+    myCharVector.removeAt(2);
+    myCharVector.print(" "); // B D
+    cout << "\n";  
+    myCharVector.removeAt(1);
+    myCharVector.print(" "); // B
+    cout << "\n"; 
+    myCharVector.removeAt(0);
+    myCharVector.print(" "); // 
+    cout << "\n"; 
     cout << myCharVector.toString() << "\n";
 
     return 0;
