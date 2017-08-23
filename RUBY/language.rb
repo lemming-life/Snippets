e3ffeeabe3cc6ccdbd087250425312be51642471
lemming-life <email@lemming.life>
2017-08-22 21:23:24 -0600

    get string from console

--- RUBY/language.rb
+++ RUBY/language.rb
@@ -13,25 +13,22 @@
 print "Hello, World\n"  # print does not add a \n
 puts "Hello, World"     # puts always adds a \n
 
-
-# Get from console example
-# print "Enter an integer: "
-# number = gets.to_i                  # Get from console, convert to integer
-# puts "You entered " + number.to_s   # Output to console the concatenation of the literal and the converted number to string
-
 # Arithmetic: +, -, *, /, %, +=, -=
 
 # Everything is an object in Ruby
 # - The type is determined by Ruby
 # - note the lower case for variable name
-number = 1      # Fixnum 
+number = 1       
 float = 0.123   # Floats must have zero prior to the decimal.  Recall that there are limits on float (14 digits)
 string = "This is a string"
+bool = true     
 
 # Get the class name by doing .class
-number.class    # Fixnum
-float.class     # Float
-string.class    # String
+number.class       # Integer
+float.class        # Float
+string.class       # String
+bool.class         #TrueClass
+
 
 # Constant
 # - First letter in upper case
@@ -40,6 +37,10 @@ MATH_PI = 3.14
 
 # Boolean operators: >, >=, <, <=, ==, != 
 # Logical operators: and, &&, or, ||, not, !
+# Comparison with <=> will yield -1, 0, or 1 depending on left and right hand variables
+# 1 <=> 2   # -1
+# 3 <=> 3   # 0
+# 3 <=> 2   # 1
 
 # Conditional: if, elsif, els
 if (2 > 3 && 5 < 3)
@@ -50,6 +51,17 @@ else
     # This will run
 end
 
+# Conditional: unless
+unless 2 < 4
+    # This will not run
+else
+    # This will run
+end
+
+# Evaluate if condition is true
+number = 2
+number = 5 if 3 > 2     # number is now equal to 5
+# puts number.to_s      # 5
 
 # Write File
 file_out = File.new("test.txt", "w")
@@ -64,4 +76,20 @@ File.delete("test.txt")
 
 # Load and execute from other file
 load "language_other_file.rb" # Executing language_other_file.rb
+
+
+# Get number from console example
+=begin
+print "Enter an integer: "
+number = gets.to_i                  # Get from console, convert to integer
+puts "You entered " + number.to_s   # Output to console the concatenation of the literal and the converted number to string
+=end
+
+# Get string from console example
+=begin
+print "Enter your name: "
+name = gets.chomp  # chomp removes the \n
+puts "You entered " + name
+=end
+
 
