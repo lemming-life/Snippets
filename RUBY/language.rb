342e1c0e6c6f7f0557ef65e35464f37aa1ae5bab
lemming-life <email@lemming.life>
2017-08-22 20:48:43 -0600

    Added language.rb

---
+++ RUBY/language.rb
@@ -0,0 +1,8 @@
+# Author: http://lemming.life
+# Language: D
+
+# This is a comment
+
+# Output to console
+print "Hello, World\n"  # print does not add a \n
+puts "Hello, World"     # puts always adds a \n
