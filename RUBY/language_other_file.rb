8ef1bf252dd1670490a71d439f06947059743764
lemming-life <email@lemming.life>
2017-08-24 22:37:31 -0600

    Added inheritance example.

--- RUBY/language_other_file.rb
+++ RUBY/language_other_file.rb
@@ -4,8 +4,8 @@ puts "Executing language_other_file.rb"
 # A class
 class Shape
     def initialize
-        @x = 0
-        @y = 0
+        @x = 0  # member variable
+        @y = 0  # member variable
     end
 
     def set_x(new_x)
@@ -24,19 +24,53 @@ class Shape
         return @y   # explicit return
     end
 
-    # setter (can be used to verify that data compatible)
+    # Setter (can be used to verify that data compatible)
     def x=(new_x)
         @x = new_x if new_x.is_a?(Numeric)
     end
 
-    # a getter
+    # Getter
     def x
         @x  # implicit return
     end
 
+    def y=(new_y)
+        @y = new_y if new_y.is_a?(Numeric)
+    end
+
+    def y
+        @y
+    end
+
+    def get_area
+        return 0   
+    end
 end
 
 # Create an object
 shape = Shape.new
-shape.x = 5
-puts shape.get_x
+shape.set_x(1)      # Use a method, pass a value
+shape.get_x         # Use a method to get
+shape.x = 5         # Use the setter
+shape.x        # Use the getter
+
+
+# Class using inheritance
+class Circle < Shape
+    #attr_reader :radius, :area # Getters
+    #attr_writer :radius :area  # Setters
+    attr_accessor :radius, :color # Both getter and setter
+
+    # Override get_area of Shape
+    def get_area
+        return 3.14 * radius
+    end
+
+end
+
+
+circle = Circle.new
+circle.x = 1    # Uses the x setter from Shape
+circle.y = -1   
+circle.radius = 2
+circle.get_area # 6.28
